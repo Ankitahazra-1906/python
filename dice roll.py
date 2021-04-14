51ed68887806ea70af76bbbc4b6169512b1f7ac9
Ankita Hazra <ankitahazra2000@gmail.com>
2021-04-14 19:15:54 +0530

    Add files via upload

---
+++ dice roll.py
@@ -0,0 +1,59 @@
+import random
+a="b"
+while a=="b":
+    num=random.randint(1,6)
+    if num==1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    if num==2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+        print("[-----]")
+    if num==3:
+        print("[-----]")
+        print("[     ]")
+        print("[ 000 ]")
+        print("[     ]")
+        print("[-----]")
+    if num==4:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    if num==5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if num==6:
+        print("[-----]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[-----]")
+    a=input("Press b to roll again and e to exit: ")
+    print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
