a57ad32f9cd854a1b383df3a14c2f0f88dbfefc7
Ankita Hazra <ankitahazra2000@gmail.com>
2021-04-14 19:25:56 +0530

    Add files via upload

---
+++ normal calculations with python.py
@@ -0,0 +1,18 @@
+val1=float(input("Enter the first number:"))
+op=input("Enter the operator:")
+val2=float(input("Enter the second number:"))
+if op=='+':
+    print(val1+val2)
+elif op=='-':
+    print(val1-val2)
+elif op=='*':
+    print(val1*val2)
+elif op=='/':
+    print(val1/val2)
+else:
+    print("invalid")
+
+
+           
+
+
